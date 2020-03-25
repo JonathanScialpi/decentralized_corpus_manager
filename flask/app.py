c8d89e5ddd08bc7a11fad06835a27e8ddd3ce81f
Jonathan Scialpi <scialpi27@gmail.com>
2020-03-25 16:47:53 -0400

    making sure that all measurements are converted to doubles

--- flask/app.py
+++ flask/app.py
@@ -12,13 +12,48 @@ app = Flask(__name__)
 def home():
     return "Hello From flask > app.py..."
 
-
 @app.route("/classify", methods=['POST'])
 def passive_aggressive_classifier():
+    #orig_file = open(request.json["path_to_file"], "r", encoding="utf8")
+    #lines = orig_file.readlines()
+
+    #loop through csv to build training_data and target_label_array
+    training_data = []
+    target_label_array = []
+    for k,v in request.json["corpus"]:    
+        training_data.append(k)
+        target_label_array.append(v.replace('\n',''))
+    target_label_array = asarray(target_label_array) #set to numpy array
+
+    #Seperate data into training and testing sets (30%)
+    X_train, X_test, y_train, y_test = train_test_split(training_data, target_label_array, test_size = 0.3, random_state = 42)
+
+    #Setup training data to PassiveAgressiveClassifier Pipeline
+    text_clf = Pipeline([
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', PassiveAggressiveClassifier()),
+    ])
+
+    text_clf.fit(X_train, y_train)  
+    predicted = text_clf.predict(X_test)
+    #mean_output = mean(predicted == y_test)
+
+    classification_report_output = classification_report(y_test, predicted,output_dict=True, target_names=request.json["list_of_goals"])
+
+    for key in classification_report_output.keys():
+        classification_report_output[key]["support"] = float(classification_report_output[key]["support"])
+                                            
+    #confusion_matrix_output = confusion_matrix(y_test, predicted)
+    #accuracy_output = accuracy_score(predicted, y_test)
+
+    return jsonify(classification_report_output)
+
+@app.route("/classify_with_csv", methods=['POST'])
+def pac_with_csv():
     orig_file = open(request.json["path_to_file"], "r", encoding="utf8")
     lines = orig_file.readlines()
 
-    #loop through csv to build training_data and target_label_array
     training_data = []
     target_label_array = []
     for line in lines:
@@ -42,6 +77,10 @@ def passive_aggressive_classifier():
     #mean_output = mean(predicted == y_test)
 
     classification_report_output = classification_report(y_test, predicted,output_dict=True, target_names=request.json["list_of_goals"])
+
+    for key in classification_report_output.keys():
+        if isinstance(classification_report_output[key], dict) and "support" in classification_report_output[key].keys():
+            classification_report_output[key]["support"] = float(classification_report_output[key]["support"])
                                             
     #confusion_matrix_output = confusion_matrix(y_test, predicted)
     #accuracy_output = accuracy_score(predicted, y_test)
