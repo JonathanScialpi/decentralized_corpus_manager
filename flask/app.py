424c91fb030bcd8335a13082d4e881d916b21d7f
Jonathan Scialpi <scialpi27@gmail.com>
2020-04-02 11:28:17 -0400

    Casted all scores to floats/doubles and also normalized the accuracy object

--- flask/app.py
+++ flask/app.py
@@ -14,16 +14,17 @@ def home():
 
 @app.route("/classify", methods=['POST'])
 def passive_aggressive_classifier():
-    #orig_file = open(request.json["path_to_file"], "r", encoding="utf8")
-    #lines = orig_file.readlines()
-
+    
     #loop through csv to build training_data and target_label_array
     training_data = []
     target_label_array = []
-    for k,v in request.json["corpus"]:    
+    list_of_goals = []
+    for k,v in request.json["corpus"].items():    
         training_data.append(k)
-        target_label_array.append(v.replace('\n',''))
+        target_label_array.append(v)
+    list_of_goals = target_label_array
     target_label_array = asarray(target_label_array) #set to numpy array
+    
 
     #Seperate data into training and testing sets (30%)
     X_train, X_test, y_train, y_test = train_test_split(training_data, target_label_array, test_size = 0.3, random_state = 42)
@@ -37,16 +38,17 @@ def passive_aggressive_classifier():
 
     text_clf.fit(X_train, y_train)  
     predicted = text_clf.predict(X_test)
-    #mean_output = mean(predicted == y_test)
 
-    classification_report_output = classification_report(y_test, predicted,output_dict=True, target_names=request.json["list_of_goals"])
+    classification_report_output = classification_report(y_test, predicted,output_dict=True, target_names=list(set(list_of_goals)))
+    
+    #normalize accuracy object
+    classification_report_output["accuracy"] = {"score": classification_report_output["accuracy"]}
 
+    #cast all scores to doubles/floats
     for key in classification_report_output.keys():
-        classification_report_output[key]["support"] = float(classification_report_output[key]["support"])
-                                            
-    #confusion_matrix_output = confusion_matrix(y_test, predicted)
-    #accuracy_output = accuracy_score(predicted, y_test)
-
+        if "support" in classification_report_output[key].keys():
+            classification_report_output[key]["support"] = float(classification_report_output[key]["support"])
+    
     return jsonify(classification_report_output)
 
 @app.route("/classify_with_csv", methods=['POST'])
