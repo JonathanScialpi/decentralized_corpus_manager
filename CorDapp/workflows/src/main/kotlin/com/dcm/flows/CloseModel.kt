c05b81db51f84a2071245329ade619b95a61b5a4
Jonathan Scialpi <scialpi27@gmail.com>
2020-04-06 09:48:36 -0400

    Created a flow that gives the owner of a model the option to exit the state and prevent any future updates to it.

---
+++ CorDapp/workflows/src/main/kotlin/com/dcm/flows/CloseModel.kt
@@ -0,0 +1,73 @@
+package com.dcm.flows
+
+import co.paralleluniverse.fibers.Suspendable
+import com.dcm.contract.ModelContract
+import com.dcm.states.ModelState
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import net.corda.core.contracts.Requirements.using
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.contracts.requireThat
+import net.corda.core.flows.*
+import net.corda.core.identity.Party
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.QueryCriteria
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+
+@InitiatingFlow
+@StartableByRPC
+
+class CloseModelFlow(
+private val modelLinearId: UniqueIdentifier
+
+): FlowLogic<SignedTransaction>() {
+    @Suspendable
+    override fun call(): SignedTransaction {
+
+        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+        val transactionBuilder = TransactionBuilder(notary)
+
+        // get current model state and use as input state
+        val queryCriteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(modelLinearId))
+        val modelStateAndRef =  serviceHub.vaultService.queryBy<ModelState>(queryCriteria).states.single()
+        transactionBuilder.addInputState(modelStateAndRef)
+        val inputModelState = modelStateAndRef.state.data
+
+        if(ourIdentity != inputModelState.owner){
+            throw IllegalArgumentException("Only the owner can close a model.")
+        }
+
+        // finish building tx without an output state
+        val commandData = ModelContract.Commands.CloseModel()
+        transactionBuilder.addCommand(commandData, inputModelState.participants.map { it.owningKey })
+        transactionBuilder.verify(serviceHub)
+
+        // sign and get other signatures
+        val ptx = serviceHub.signInitialTransaction(transactionBuilder)
+        val sessions = (inputModelState.participants - ourIdentity).map { initiateFlow(it) }.toSet()
+        val stx = subFlow(CollectSignaturesFlow(ptx, sessions))
+        return subFlow(FinalityFlow(stx, sessions))
+    }
+
+
+}
+
+@InitiatedBy(CloseModelFlow::class)
+class CloseModelFlowResponder(val counterpartySession: FlowSession): FlowLogic<SignedTransaction>() {
+
+    @Suspendable
+    override fun call(): SignedTransaction {
+        val signedTransactionFlow = object : SignTransactionFlow(counterpartySession) {
+            override fun checkTransaction(stx: SignedTransaction) = requireThat {
+            }
+        }
+
+        val txWeJustSignedId = subFlow(signedTransactionFlow)
+        return subFlow(ReceiveFinalityFlow(otherSideSession = counterpartySession, expectedTxId = txWeJustSignedId.id))
+    }
+}
